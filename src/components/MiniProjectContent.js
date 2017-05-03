adb94317a801904ee6c6376a13a0468e869cd56f
Richard Mands <richard@eduku.org>
2017-05-03 18:47:01 +0800

    update

--- src/components/MiniProjectContent.js
+++ src/components/MiniProjectContent.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import {Link} from 'react-router';
-import Board from './Board'
+import Board from './Board';
 
-const MiniProjectContent = () => {
+class MiniProjectContent extends React.Component {
+
+  constructor() {
+  super()
+  this.state = {
+    knightPosition: [0,0]
+  }
+}
+render() {
+
+  setInterval(() =>
+  {
+    this.setState ({
+      knightPosition : [
+        Math.floor(Math.random() * 8),
+        Math.floor(Math.random() * 8)
+      ]
+    })
+  }
+  , 10000);
+
+
   return (
     <div className="miniProjectContent">
-      <Board knightPosition={[0, 0]} />
+      <Board knightPosition={this.state.knightPosition} />
     </div>
   );
-};
+}
+}
 
 export default MiniProjectContent;
