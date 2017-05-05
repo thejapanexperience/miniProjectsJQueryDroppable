c5cbec536f1df8a4b79b9cbdd6b9c9a8fad0bad5
Richard Mands <richard@eduku.org>
2017-05-05 15:56:53 +0800

    touch

--- src/components/MiniProjectContent.js
+++ src/components/MiniProjectContent.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
+import TouchBackend from 'react-dnd-touch-backend';
+import MultiBackend, {Preview, TouchTransition} from 'react-dnd-multi-backend';
+// import HTML5toTouch from 'react-dnd-multi-backend/lib/HTML5toTouch'; // or any other pipeline
 import Target1 from './Target1';
 import Target2 from './Target2';
 import TargetInactive from './TargetInactive';
 
+const HTML5toTouch = {
+  backends: [
+    {
+      backend: HTML5Backend
+    },
+    {
+      backend: TouchBackend({enableMouseEvents: true}), // Note that you can call your backends with options
+      preview: true,
+      transition: TouchTransition
+    }
+  ]
+};
 
-@DragDropContext(HTML5Backend)
+
+// @DragDropContext(HTML5Backend)
+@DragDropContext(MultiBackend(HTML5toTouch))
 export default class MiniProjectContent extends React.Component {
 
   constructor() {
     super()
   }
+
+
+  generatePreview(type, item, style) {
+    let itemPreview = item;
+    console.log('itemPreview: ', itemPreview)
+    console.log('type: ', type)
+    console.log('item: ', item)
+    console.log('style: ', style)
+    Object.assign(style, {backgroundColor: item.color, width: '50px', height: '50px'});
+    return <div style={style}>Generated</div>
+    // return itemPreview;
+  }
+
 
   render() {
 
@@ -21,6 +51,8 @@ export default class MiniProjectContent extends React.Component {
       <div className="dragAround" id="dragAround">
         <Target1 box={'BOXONE'} id={'target1'} backgroundColor={'pink'} width={'50%'} left={'125%'} top={'25%'} title={'Drag me to the pink box'} />
         <TargetInactive id={'target2'} backgroundColor={'orange'} width={'50%'}/>
+        <Preview generator={this.generatePreview} />
+
       </div>
 
       <div className="dragAround" id="dragAround">
