8038eb27affc509048315278b2051556b5b53879
Richard Mands <richard@eduku.org>
2017-05-11 16:40:48 +0800

    run from dist

--- tools/build.js
+++ tools/build.js
@@ -5,26 +5,26 @@ import webpack from 'webpack';
 import config from '../webpack.config.prod';
 import {chalkError, chalkSuccess, chalkWarning, chalkProcessing} from './chalkConfig';
 
-process.env.NODE_ENV = 'production'; // this assures React is built in prod mode and that the Babel dev config doesn't apply.
+// process.env.NODE_ENV = 'production'; // this assures React is built in prod mode and that the Babel dev config doesn't apply.
 
 console.log(chalkProcessing('Generating minified bundle. This will take a moment...'));
 
 webpack(config).run((error, stats) => {
-  if (error) { // so a fatal error occurred. Stop here.
-    console.log(chalkError(error));
-    return 1;
-  }
+  // if (error) { // so a fatal error occurred. Stop here.
+  //   console.log(chalkError(error));
+  //   return 1;
+  // }
 
   const jsonStats = stats.toJson();
 
-  if (jsonStats.hasErrors) {
-    return jsonStats.errors.map(error => console.log(chalkError(error)));
-  }
+  // if (jsonStats.hasErrors) {
+  //   return jsonStats.errors.map(error => console.log(chalkError(error)));
+  // }
 
-  if (jsonStats.hasWarnings) {
-    console.log(chalkWarning('Webpack generated the following warnings: '));
-    jsonStats.warnings.map(warning => console.log(chalkWarning(warning)));
-  }
+  // if (jsonStats.hasWarnings) {
+  //   console.log(chalkWarning('Webpack generated the following warnings: '));
+  //   jsonStats.warnings.map(warning => console.log(chalkWarning(warning)));
+  // }
 
   console.log(`Webpack stats: ${stats}`);
 
